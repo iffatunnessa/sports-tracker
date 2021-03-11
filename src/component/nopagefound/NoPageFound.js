61f7d0d7b68db7d863dfdc4362fbb3f9495ccbc4
iffatunnessa <45840519+iffatunnessa@users.noreply.github.com>
2021-03-11 23:38:03 +0600

    last

--- src/component/nopagefound/NoPageFound.js
+++ src/component/nopagefound/NoPageFound.js
@@ -1,11 +1,13 @@
+import { Container } from '@material-ui/core';
 import React from 'react';
-import {Container} from '@material-ui/core';
 
 const NoPageFound = () => {
     return (
-        <Container style = {{color:'white', textAlign:'center'}}>
-        <h1> Page not found</h1>
-        </Container>
+        <div>
+            <Container maxWidth="md" style={{color:'white', textAlign:'center'}}>
+                <h1>Page not found</h1>
+            </Container>
+        </div>
     );
 };
 
